9bf5f5b6aaa46746b431383fa64f58405b5f2e27
KsRyY <andy200511@126.com>
2020-04-05 20:40:27 +0800

    :recycle: Simplifying database structure

--- middleware/database.js
+++ middleware/database.js
@@ -5,27 +5,32 @@ const sub = require('subleveldown')
 const db = level('pheasant-data')
 
 /**
- * This is the sub database that stores the user posts. The key is the id of the post. The id is a hashesd combination of author + time + post contnent. The value is the post object. Its structure are as follow:
+ * This is the sublevel that stores the user posts. The key is the id of the post. The id is a hashesd combination of author + time + post contnent. The value is the post object. Its structure are as follow:
  * ```typescript
  * interface Post {
- *   author: string,
- *   contnent: string,
- *   pubDate: Date,
- *   channel: string,
+ *   author: string
+ *   contnent: string
+ *   pubDate: Date
+ *   channel: string
  *   id: string
  * }
  * ```
  */
 const posts = sub(db, 'posts', { valueEncoding: 'json' })
 
-// Sub database for storing user information
+/**
+ * This is a sublevel that stores user informations. The key is the id of the user (SHA-256 hashed email address). The value is the user object. Its structure are as follow:
+ * ```typescript
+ * interface User {
+ *   email: string
+ *   password: string // This is the salted password hash, using the SHA-256 algorithm
+ *   token: string
+ * }
+ * ```
+ */
 const users = sub(db, 'users', { valueEncoding: 'json' })
-
-// Sub database for storing user auth tokens
-const tokens = sub(db, 'tokens', { valueEncoding: 'json' })
 
 module.exports = {
   posts,
-  users,
-  tokens
+  users
 }
