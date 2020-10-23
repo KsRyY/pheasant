dae0d7aa60cf5992e6e16d8fc50243b50a44a055
unknown <25174488+KsRyY@users.noreply.github.com>
2020-10-23 22:16:49 +0800

    style: format generated code using XO

--- backend/encrypt.js
+++ backend/encrypt.js
@@ -1,28 +1,28 @@
 const CryptoJS = require('crypto-js')
 
 function saltPass(pwd) {
-    const salt = CryptoJS.lib.WordArray.random(256 / 8) // create a 64-bype salt
-    const salted = CryptoJS.PBKDF2(pwd, salt, {
-        keySize: 512 / 32,
-        iterations: 64
-    }) // salt the key with 512-bit size and 64 iterations
+	const salt = CryptoJS.lib.WordArray.random(256 / 8) // Create a 64-bype salt
+	const salted = CryptoJS.PBKDF2(pwd, salt, {
+		keySize: 512 / 32,
+		iterations: 64
+	}) // Salt the key with 512-bit size and 64 iterations
 
-    return { salt, salted }
+	return { salt, salted }
 }
 
 function resaltPass(pwd, salt) {
-    const salted = CryptoJS.PBKDF2(pwd, salt, {
-        keySize: 512 / 32,
-        iterations: 64
-    }) // resalt the key with 512-bit size and 64 iterations, using the previously used salt
+	const salted = CryptoJS.PBKDF2(pwd, salt, {
+		keySize: 512 / 32,
+		iterations: 64
+	}) // Resalt the key with 512-bit size and 64 iterations, using the previously used salt
 
-    return { salted }
+	return { salted }
 }
 
 function getUniqueID(email, saltedKey) {
-    const uniqueID = CryptoJS.SHA3(email + saltedKey) // cauculate the SHA3 Hash of email and the salted key as unique id
+	const uniqueID = CryptoJS.SHA3(email + saltedKey) // Cauculate the SHA3 Hash of email and the salted key as unique id
 
-    return uniqueID
+	return uniqueID
 }
 
 module.exports = { saltPass, resaltPass, getUniqueID }
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,25 @@ const level = require('level')
 const sublevel = require('subleveldown')
 const os = require('os')
 const path = require('path')
+const encrypt = require('./encrypt')
 
 // Open a database for storage, default path is `~/.pheasant/db`
 const database = level(path.join(os.homedir(), '.pheasant', 'db'), {
-    // encode value using JSON
-    valueEncoding: 'json'
+	// Encode value using JSON
+	valueEncoding: 'json'
+})
+
+/* Create a sublevel for storing user information.
+ * The key is each users' unique ID (refer to the `getUniqueId` method defined in encrypt.js)
+ * The value conforms to the following format:
+ * ```
+ * {
+ *   username: string,
+ *   nickname: string
+ *   email: string
+ * }
+ * ```
+ */
+const users = sublevel(database, 'users', {
+	valueEncoding: 'json'
 })
